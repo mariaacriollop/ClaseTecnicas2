83816008c36dd1e67eeb1c2ed8f0d5863225e79c
María Criollo <mariaa.criollop@konradlorenz.edu.co>
2023-08-19 10:07:15 -0500

    Primera estructura del proyecto

---
+++ TecnicasProgramacion2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TecnicasProgramacion2
+{
+    internal class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+          
+
+        }
+    }
+}
